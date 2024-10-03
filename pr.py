45dfc515cba542f87dd7c041a0a113f90e4ece66
StihEvgenia <stihurovaevgenia@gmail.com>
2024-10-03 23:50:05 +0700

    Add files via upload

---
+++ pr.py
@@ -0,0 +1,5 @@
+homework=12
+hours_spent=1.5
+course_name="Python"
+executin_time=(hours_spent/homework)
+print('Курс:',course_name,',','всего задач:',homework,',','затрачено часов:',hours_spent,',','среднее время выполнения:',(hours_spent/homework),'часа')
