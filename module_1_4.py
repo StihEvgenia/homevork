a87b4b87707f0a63717c45cd455dd1d9b47dd30a
StihEvgenia <stihurovaevgenia@gmail.com>
2024-10-04 18:50:00 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+my_string=input("Как тебя зовут? ")
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print('как твоё настроение?'.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
+
+
