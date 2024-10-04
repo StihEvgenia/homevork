279b9f1ab66817a8090f67899e269e2f5cb90a7d
StihEvgenia <stihurovaevgenia@gmail.com>
2024-10-04 22:18:55 +0700

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var=1,2,'кит', 'кот'
+print(immutable_var)
+#tuple[0]=5 объект не может быть изменен
+mutable_list =['book','copybook','tabler']
+mutable_list.append('lamp')
+print(mutable_list)
+mutable_list.remove('book')
+print(mutable_list)
